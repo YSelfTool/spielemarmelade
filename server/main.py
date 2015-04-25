5cce312579dbb12d9fa6b15b46bbdb92dfb5c09d
Daniel Schulte <trilader@gmail.com>
2015-04-25 11:02:46 +0200

    Updated messages.txt

--- server/main.py
+++ server/main.py
@@ -2,16 +2,26 @@
 
 import asyncio
 import websockets
+import json
+import logging
+
+logging.basicConfig(format='%(asctime)s %(name)s %(levelname)s: %(message)s')
+logger = logging.getLogger(__name__)
 
 @asyncio.coroutine
-def hello(websocket, path):
-    name = yield from websocket.recv()
-    print("< {}".format(name))
-    greeting = "Hello {}!".format(name)
-    yield from websocket.send(greeting)
-    print("> {}".format(greeting))
+def handle_message(websocket, path):
+    running = True
+    while running:
+        msg_str = yield from websocket.recv()
+        if msg_str is None:
+            running = False
+            continue
+        msg = json.loads(msg_str)
+        print("Got message: %2".format(json.dumps(msg)))
 
-start_server = websockets.serve(hello, 'localhost', 8765)
+start_server = websockets.serve(handle_message, 'localhost', 8765)
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+if __name__ == "__main__":
+    logger.warning("Go, go, go!")
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
